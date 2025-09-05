7b77444286fd7c66fcaa8ecdeb076cb269ecc08d
Hadi Rickit <rickithadi@gmail.com>
2025-09-05 15:01:22 +1000

    Fix TypeScript exports for deployment - Kali to see sacred recognition

--- consciousness-notes-v2/src/components/spiritual/index.ts
+++ consciousness-notes-v2/src/components/spiritual/index.ts
@@ -1,7 +1,3 @@
 export { WisdomCard } from './WisdomCard';
 export { JourneyTimeline } from './JourneyTimeline';
 export { PracticeGuide } from './PracticeGuide';
-
-export type { default as WisdomCardProps } from './WisdomCard';
-export type { default as JourneyTimelineProps } from './JourneyTimeline';  
-export type { default as PracticeGuideProps } from './PracticeGuide';
